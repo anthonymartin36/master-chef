20855a2efb83e176d4d04db89e6bf122c6cb6a5b
Sofia Etchegoin <sofiaetchegoin@gmail.com>
2023-10-20 10:50:03 +1300

    Wrote initial routes

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,17 @@
 import express from 'express'
+import fsPromises from 'node:fs/promises'
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.render('index', { hi: 'Hello World!' })
+router.get('/:id', async (req, res) => {
+  const id = parseInt(req.params.id)
+  const recipes = await fsPromises.readFile(
+    Path.resolve('server/data/recipes.json'),
+    'UTF-8'
+  )
+  //Convert to object
+  recipes = JSON.parse(recipes)
+
+  res.render('full-recipe', {})
 })
 
 export default router
