ac38d0dde00db73685d4653d0321d51be921c556
anthony-martin <27912970+anthonymartin36@users.noreply.github.com>
2023-10-20 11:07:50 +1300

    String in recipes.json to integer

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import * as Path from 'node:path'
 import fsPromises from 'node:fs/promises'
 import routes from './routes.js'
 
+
 //Middleware
 
 const server = express()
@@ -12,6 +13,10 @@ server.set('view engine', 'hbs')
 server.set('views', Path.resolve('server/views'))
 server.use(express.urlencoded({ extended: true }))
 server.use('/', routes)
+//server configuration
+const publicFolder = Path.resolve('public')
+server.use(express.static(publicFolder))
+server.use(express.urlencoded({ extended: false }))
 
 server.get('/', async (req, res) => {
   const recipes = await fsPromises.readFile(
