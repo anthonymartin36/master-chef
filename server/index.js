f8772b8199b88edf937150e5b20d9ab2ddeb99fb
anthonymartin36 <anthony.martin36@gmail.com>
2024-03-08 13:20:03 +1300

    removing env

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import server from './server.js'
 //const server = require('./server.js')
 
-const PORT = import.meta.env.PORT || 10000
+const PORT = process.env.PORT || 10000
 
 server.listen(PORT, function () {
   console.log('Listening on port', PORT)
