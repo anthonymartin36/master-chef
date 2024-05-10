593675fafa50b4c74c5a637c4bba5a8920971e3e
anthonymartin36 <anthony.martin36@gmail.com>
2024-05-10 15:05:25 +1200

    handlebars added

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 // vite.config.js
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+const handlebars = require('handlebars')
 
 export default defineConfig({
   plugins: [handlebars()],
